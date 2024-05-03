1772b0f389940150d1fd0aa27899c0fbcbb9da98
AresEros <iamkhyle@outlook.com>
2024-05-03 07:21:24 -0700

    first commit

---
+++ basic.js
@@ -0,0 +1,6 @@
+let bb = "JavaScript code"
+console.log(bb)
+
+const kels = document.getElementById('kelechi')
+kels.innerHTML = bb
+//console.log(kels.textContent)
---
+++ script.js
@@ -0,0 +1,70 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?port_by=popularity.desc&api_key=6fa4c167ad7af1cfe08cc2559a81c70e&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=6fa4c167ad7af1cfe08cc2559a81c70e&quary=";
+
+const main = document.getElementById("section")
+const form = document.getElementById("form")
+const search = document.getElementById("query")
+
+returnMovies(APILINK)
+function returnMovies(url){
+    fetch(url, {
+    body: null,
+    method: 'GET',
+    }).then((res) => res.json()).then((data) =>{
+        console.log(data.results);
+        data.results.forEach((element)=>{
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class','card');
+
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class','row');
+
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class','column');
+
+        const image = document.createElement('img');
+        image.setAttribute('class','thumbnail');
+        image.setAttribute('id','image');
+
+        const title = document.createElement('h3');
+        div_card.setAttribute('id','title');
+
+        const center = document.createElement('center');
+
+        title.innerHTML = `${element.title}`;
+        image.src = IMG_PATH + element.poster_path;
+
+        center.appendChild(image);
+        div_card.appendChild(center);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
+
+        main.appendChild(div_row);
+        })
+        
+    })
+
+
+    }
+
+
+    form.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        main.innerHTML = ''
+
+
+        const searchItem = search.value;
+
+        if (searchItem) {
+          returnMovies(SEARCHAPI + searchItem);
+          search.value = "";  
+        }
+    })
+   
+
+
+
+
+    
